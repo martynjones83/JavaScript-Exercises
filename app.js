92531b047028bd5b42aa37fbc6a5680024468480
martynjones83 <114222235+martynjones83@users.noreply.github.com>
2022-10-14 20:52:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// Exercise 1 - Age Logger //
+
+let myAge = "38";
+console.log(myAge);
+
+
+// Exercise 2 - So Many Rabbits //
+
+let numberOfRabbits = 2;
+console.log(numberOfRabbits * 3);
+
+
+// Number Exercise 3 - BODMAS //
+
+let bodmasExample = 3;
+console.log((bodmasExample + 5) / 6);
+
+
+// Number Exercise 4 - Random Number Generator //
+
+let randomInteger = Math.floor(Math.random() * 6 + 1);
+console.log(randomInteger);
+
+
+// String Exercise 1 - Log Your Country //
+
+let myCountry = "England";
+console.log(myCountry);
+
+
+// String Exercise 2 - Stringe Length //
+
+let messageLength = "England";
+console.log(messageLength.length);
+
+
+// String Exercise 3 - Upper Case //
+
+let favouriteFood = "Roast Dinner";
+console.log(favouriteFood.toUpperCase());
+
+
+// String Exercise 4 - Lower Case //
+
+let favouriteFood2 = "Roast Dinner";
+console.log(favouriteFood.toLowerCase());
+
+
+// String Exercise 5 - Concatenation //
+
+let myName = "Martyn Jones";
+let myAge2 = "38";
+let appendix = "What is your name?"
+console.log("Hello my name is " + myName + " and I am " + myAge2 + " years old. " + appendix);
+
+
+
+// Exercise 6 - Template Literals - Try If You Want //
+
+let firstName = "Martyn";
+let lastName = "Jones";
+console.log(`Welcome ${firstName} ${lastName}, 
+How can I help you?`);
